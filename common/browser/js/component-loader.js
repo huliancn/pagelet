29c42020e62a8cd9a78aec3a662ac30c03d0b68d
hulian <hu.lian.cn@gmail.com>
2018-04-26 12:41:32 +0800

    init

--- common/browser/js/component-loader.js
+++ common/browser/js/component-loader.js
@@ -8,11 +8,18 @@ require.config({
 });
 define(['jquery'],function($){
     
-
+    //动态加载css
     $('[component]').each(function(){
         var component=$(this)
-        component.load('/components/'+component.attr('component')+'/browser/index.html');
+        $('head').append('<link rel="stylesheet" type="text/css" href="'+'/components/'+component.attr('component')+'/browser/css/index.css'+'" />')
     });
 
-
+    //动态加载html
+    $('[component]').each(function(){
+        var component=$(this)
+        component.load('/components/'+component.attr('component')+'/browser/index.html',function(){
+            //加载html完成后，加载js
+            require(['/components/'+component.attr('component')+'/browser/js/index.js']);
+        });
+    });
 })
