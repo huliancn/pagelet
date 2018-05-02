64f9d68c6fe4829cdfb33de93d8983c6fe09bc84
hulian <hu.lian.cn@gmail.com>
2018-05-02 17:06:50 +0800

    init

--- common/server/db/mongodb.js
+++ common/server/db/mongodb.js
@@ -1,41 +1,54 @@
-const config = require('config');
+module.exports=function(){
 
-if(config.get('profile')==='test'){
+    const config = require('config');
+    const log = require('../log/log4js.js');
+    
+    function getDb( name,callback ){
+
+        if(config.get('profile')==='test'){
    
-    //启动内嵌数据库
-    //var Engine = require('mongodb');
-    //var server =  new Engine.Server('localhost', 27017);
-    //var db = new Engine.Db('test', server);
-    var Engine = require('tingodb')();
-    var db = new Engine.Db(global.rootPath+'/data/test', {});
-    module.exports = {
-        db: db
+            //启动内嵌数据库
+            //var Engine = require('mongodb');
+            //var server =  new Engine.Server('localhost', 27017);
+            //var db = new Engine.Db('test', server);
+            var Engine = require('tingodb')();
+            var db = new Engine.Db(global.rootPath+'/data/test', {});
+            callback(err,db);
+        
+        }else{
+        
+            //获取驱动
+            const MongoClient = require('mongodb').MongoClient;
+            // Connection URL
+            const url = config.get('production.db.url')
+           
+            // Use connect method to connect to the server
+            MongoClient.connect(url, function (err, client) {
+        
+                if (err) {
+                    log.server.error(err);
+                    callback(err);
+                    return;
+                }
+        
+                console.log("Connected successfully to server");
+        
+                const db = client.db(name);
+        
+                console.log(db);
+                if(db===undefined){
+                    console.log('打开数据库失败！');
+                }
+        
+                callback(err,db);
+        
+            });
+        
+        }
     }
 
-}else{
+    return {
+        getDb:getDb
+    }
 
-    //获取驱动
-    const MongoClient = require('mongodb').MongoClient;
-    // Connection URL
-    const url = config.get('production.user.db.url')
-    // Database Name
-    const dbName = config.get('production.user.db.name');
-    // Use connect method to connect to the server
-    MongoClient.connect(url, function (err, client) {
-
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        console.log("Connected successfully to server");
-
-        const db = client.db(dbName);
-
-        module.exports = {
-            db: db
-        }
-
-    });
-
-}
+}();
